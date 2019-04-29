1dd36065777841803b55c77a2d13b5ebf9f6ef78
Daniel dos Santos Pereira <danield1591998@gmail.com>
2019-04-29 09:47:38 -0300

    Change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HackathonAngular'`, () => {
+  it(`should have as title 'UFSMBot-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('HackathonAngular');
+    expect(app.title).toEqual('UFSMBot-Angular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to HackathonAngular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to UFSMBot-Angular!');
   });
 });

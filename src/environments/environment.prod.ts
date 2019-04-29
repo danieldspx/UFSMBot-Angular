15adae11a8c110667e9f647088467335c43c61df
Daniel dos Santos Pereira <danield1591998@gmail.com>
2019-04-29 01:39:16 -0300

    * Firebase setup to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCi0_QekiFVXLYTxARrr9phyjLA2xmicFg",
+    authDomain: "ufsmbot.firebaseapp.com",
+    databaseURL: "https://ufsmbot.firebaseio.com",
+    projectId: "ufsmbot",
+    storageBucket: "ufsmbot.appspot.com",
+    messagingSenderId: "100941804377"
+  }
 };
